788135f16969515fe36c12e9b9beb69d0ff3dfd0
Jay Raval <jay@jays-macbook-pro.local>
2019-01-28 00:22:45 -0500

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Jay Raval on 1/27/19.
+//  Copyright © 2019 Jay Raval. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+    
+}
